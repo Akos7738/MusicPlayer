a9c95e66ba06f3f0dd751641d2e2b3814dc19f4b
Akos 7738 <czmorekakos@gmail.com>
2026-07-04 23:46:56 +0200

    Gui added

---
+++ src/me/akos/musicplayer/App.java
@@ -0,0 +1,9 @@
+package me.akos.musicplayer;
+
+import com.mpatric.mp3agic.Mp3File;
+
+public class App {
+    static void main() {
+
+    }
+}
---
+++ src/me/akos/musicplayer/GUI.java
@@ -0,0 +1,9 @@
+package me.akos.musicplayer;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+    public GUI() {
+
+    }
+}
